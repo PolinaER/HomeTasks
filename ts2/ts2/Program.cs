af03c5716ea2be1020ee092a808fe707dd8c8834
Polina.Ershova@urfu.me <Polina.Ershova@urfu.me>
2021-10-24 16:33:36 +0500

    task 2

---
+++ ts2/ts2/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace ts2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int a = 2, b = 3, c = 5;
+            int V = a * b * c;
+            int SPov = 2 * a * b + 2 * b * c + 2 * a * c;
+
+            Console.WriteLine($"Прямоугольный параллелипипед со сторонами {a}, {b}, {c}  имеет объём: {V} и площадь поверхности: {SPov}");
+
+            Console.ReadKey();
+        }
+    
+    }
+}
