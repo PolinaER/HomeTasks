ceabdb4739f7e1724135394eceb155566b6f8022
Polina.Ershova@urfu.me <Polina.Ershova@urfu.me>
2021-10-27 00:13:15 +0500

    Task 3

---
+++ ts3/ts3/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace ts3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите число - значение x");
+            var x = double.Parse(Console.ReadLine());
+            FindMod3(x);
+
+            Console.ReadKey();
+        }
+
+        static void FindMod3(double x)
+        {
+
+            Console.WriteLine(Math.Abs(x + Math.Abs(x + Math.Abs(x + Math.Sqrt(x + 1)))) + 
+                                  " = |" + x + "+|" + x + "+|" + x + "+√(" + x + "+1)|||");
+        }
+    }
+    
+}
