8a35142efa3d1acabd68e820ef07bcb58d479d9c
Polina.Ershova@urfu.me <Polina.Ershova@urfu.me>
2021-11-13 19:13:38 +0500

    task 7

---
+++ Task7/Task7/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace Task7
+{
+    class Program
+    {
+        static void Main()
+        {
+            Console.WriteLine("Enter value x");
+            var x = double.Parse(Console.ReadLine());
+            Console.WriteLine($"The function y({x}) = {CalculFunctVal(x)}");
+
+        }
+        static double CalculFunctVal(double x)
+        {
+           double y;
+           if (x > 1)
+               return y = Math.Log(x);
+           else
+             if (x > 0)
+                 return y = -Math.Log(x);
+             else
+                 return y = 0;         
+        }   
+    }
+}
