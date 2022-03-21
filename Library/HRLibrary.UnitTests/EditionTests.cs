9c4df28f2006cbbd8ec6303631a0b45344b79104
Polina.Ershova@urfu.me <Polina.Ershova@urfu.me>
2022-03-21 22:02:55 +0500

    task11

---
+++ Library/HRLibrary.UnitTests/EditionTests.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Library;
+using NUnit.Framework;
+
+namespace HRLibrary.UnitTests
+{
+    [TestFixture]
+    public class EditionTests
+    {
+        [Test]
+        public void ConstructorTest()
+        {
+            var comedy = CreateTestEdition();
+
+            Assert.AreEqual("Божественная комедия", comedy.Name);
+            Assert.AreEqual("Данте Алигьери", comedy.Authors);
+            Assert.AreEqual(2019, comedy.YearPublication);
+            Assert.AreEqual("АСТ Москва", comedy.PublishingHouse);
+            Assert.AreEqual(9785171001803, comedy.InventoryNumber);
+        }
+
+        [Test]
+        public void ToStringEdition_AuthorsNameInventoryName()
+        {
+            var comedy = CreateTestEdition();
+
+            Assert.AreEqual("Данте Алигьери: Божественная комедия, инвентарный номер: 9785171001803", comedy.ToString());
+        }
+
+        Edition CreateTestEdition()
+        {
+            return new Edition("Божественная комедия", "Данте Алигьери", 2019, "АСТ Москва", 9785171001803);
+        }
+    }
+}
---
+++ Library/Library/Edition.cs
@@ -0,0 +1,85 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Library
+{
+    ///Поля/свойства(точка означает, что данное свойство следует передавать в конструктор через параметр):
+    ///•название
+    ///•список авторов; 
+    ///•год издания; 
+    ///•издательство; 
+    ///•инвентарный номер(только для чтения); 
+    ///статус(в хранилище, выдана в читальный зал, выдана на дом — использовать перечисление); 
+    ///цена.
+
+    public class Edition
+    {
+        public string Name;
+        public string Authors;
+
+        int yearPublication;
+        public int YearPublication
+        {
+            get { return yearPublication; }
+            set 
+            {
+                if (value <= DateTime.Now.Year)
+                    yearPublication = value; 
+                else
+                    throw new ArgumentOutOfRangeException("Неверный год издания!");
+            }
+        }
+        public string PublishingHouse;
+        public readonly long InventoryNumber;
+        public EditionStatus Status;
+
+        int cost;
+        public int Cost
+        {
+            get { return cost; }
+            set
+            {
+                if (value > 0)
+                    cost = value; 
+                else
+                    throw new ArgumentException("Отрицательная цена!");
+            }
+        }
+
+        public Edition(string name, string authors, int yearPublication, string publishingHouse, long inventoryNumber)
+        {
+            Name = name;
+            Authors = authors;
+            YearPublication = yearPublication;
+            PublishingHouse = publishingHouse;
+            InventoryNumber = inventoryNumber;
+        }
+
+        public override string ToString()
+        {
+            return $"{Authors}: {Name}, инвентарный номер: {InventoryNumber}";
+        }
+
+        public void GetStatus()
+        {
+            Console.WriteLine(this);
+            var status = "";
+            switch (Status)
+            {
+                case EditionStatus.Home:
+                    status = "выдана на дом";
+                    break;
+                case EditionStatus.ReadingRoom:
+                    status = "в читательном зале";
+                    break;
+                case EditionStatus.Storage:
+                    status = "в хранилище";
+                    break;
+            }
+            Console.WriteLine($"В данный момент книга {status}");
+        }
+    }
+}
